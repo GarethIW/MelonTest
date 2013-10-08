ae050fbf82c352ba996522bbe5e4f8c818540cf0
Gareth Williams <gareth@garethw.net>
2013-10-08 15:41:57 +0100

    First Commit

---
+++ js/resources.js
@@ -0,0 +1,33 @@
+game.resources = [
+    { name: "tilesheet", type: "image", src: "data/map/tilesheet.png" },
+    { name: "tilesheet2", type: "image", src: "data/map/tilesheet2.png" },
+    { name: "sky-gradient", type: "image", src: "data/img/sky-gradient.jpg" },
+    { name: "head", type: "image", src: "data/img/head.png" },
+    { name: "map", type: "tmx", src: "data/map/map.tmx" }
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+	
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
