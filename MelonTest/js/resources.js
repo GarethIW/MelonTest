6790b01bea4137c180af2c59b7060f974e00fdff
Gareth Williams <gareth@garethw.net>
2013-10-19 14:23:23 +0100

    First Commit

---
+++ MelonTest/js/resources.js
@@ -0,0 +1,32 @@
+game.resources = [
+    { name: "tilesheet", type: "image", src: "data/map/tilesheet.png" },
+    { name: "sky-gradient", type: "image", src: "data/img/sky-gradient.jpg" },
+    { name: "girl", type: "image", src: "data/img/girl.png" },
+    { name: "map", type: "tmx", src: "data/map/1.tmx" }
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+	
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
